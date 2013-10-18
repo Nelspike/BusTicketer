0e1fe6b84c9c3265270caae3de0f4d30e1e12d05
Misirlou <diogotbasto@gmail.com>
2013-10-18 14:55:51 +0100

    add database creation and access, add client creation and login

--- node/server.js
+++ node/server.js
@@ -1,12 +1,13 @@
 var express = require('express'),
 	app = express(),
-	TicketsLib = require('./tickets'),
-	tickets = new TicketsLib(),
+	dbLib = require('./tickets'),
+	db = new dbLib.db("tickets.db"),
+	crypto=require('crypto'),
 	port = 81;	// Porta por defeito
 	
 //configuracao
 app.configure(function() {
-
+	app.use(express.bodyParser());
 	app.use(function (req, res, next) {
 	    res.setHeader('Server', 'TicketSystem');
 
@@ -30,6 +31,7 @@ if (args.length > 0)
 
 	p = null;
 }
+
 
 console.log('Escuta na porta: ' + port)
 app.listen(port);
@@ -59,6 +61,93 @@ function respondToJSON(req, res, out, statusCode) {
  *	Routes
  */
  
+ 
+//Criar um cliente
+//POST PARAMS: name string,nib string,pass string
+//RETURN JSON {name:<clientname>,id:<clientid>}
+app.post('/client/create',function (req,res) {
+	var client=new dbLib.Client(req.body.name);
+	if( !req.body.name ||!req.body.nib||!req.body.pass)
+		respondToJSON( req, res, {error: 'Bad request'}, 400 );
+	else
+	{
+		client.nib=req.body.nib;
+		db.createClient(client,req.body.pass, function(err, lastID, row)
+		{
+			var out = {},
+				code;
+
+			if( err )
+			{
+				code = 500;
+				out.id = -1;
+				out.error = 'Impossible to add client';
+
+				console.log('Error adding client: ' + err);
+			}
+			else
+			{
+				code = 200;
+				out.id = lastID;
+				out.name=client.name;
+				console.log('Added new client: ' + lastID+' '+client.name);
+			}
+
+			respondToJSON( req, res, out, code );
+		
+		});
+	}
+
+});
+
+//logar um cliente
+//POST PARAMS: name string,pass string
+//RETURN JSON {name:<clientname>,id:<clientid>}
+app.post('/client/login',function (req,res) {
+	if( !req.body.name ||!req.body.pass)
+		respondToJSON( req, res, {error: 'Bad request'}, 400 );
+	else
+	{
+		var name=req.body.name,pass=req.body.pass;
+		db.login(name,pass, function(err,row)
+		{
+			var out = {},
+				code;
+
+			if( err )
+			{
+				code = 500;
+				out.id = -1;
+				out.error = 'Impossible to add client';
+
+				console.log('Error login client: ' + err);
+			}
+			else
+			{
+				code = 200;
+				if (!row)
+				{
+					out.id = -1;
+					out.error = 'Wrong user or password';
+					
+					console.log('Fail login');
+				}
+				else
+				{
+				
+					out.id=row.cid;
+					out.name=row.name;
+					console.log('Login client: ' +name);
+				}
+			}
+
+			respondToJSON( req, res, out, code );
+		
+		});
+	}
+
+});
+ 
  // Retorna bilhetes de um cliente
 app.get('/list/:client', function (req, res) {
 
@@ -71,11 +160,14 @@ app.get('/list/:client', function (req, res) {
 
 	else
 	{
-		var out={client:tickets.list(client)};
+		
+		var out;
 		respondToJSON( req, res, out, 200 );
 		
 	}
 });
+
+
  
  
  app.all('*', function (req, res) {
@@ -86,5 +178,22 @@ app.get('/list/:client', function (req, res) {
 });
 
 
+/*
+ *	CONSOLE INPUT (TEST PURPOSE;REMOVE LATER)
+ */
+/* 
+ var readline = require('readline');
 
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+rl.question("reading input\n", function(answer) {
+  
+  console.log(answer );
+  if (answer=="versions") console.log(crypto.getHashes());
+  if (answer=="exit")rl.close();
+});
+
+*/
--- node/tickets.js
+++ node/tickets.js
@@ -1,18 +1,151 @@
-var fs = require("fs");
-module.exports = Ticket;
+var fs = require("fs"),
+	moment = require('moment'),
+    sqlite3 = require("sqlite3").verbose(),
+    ticketConn = undefined,
+	crypto=require('crypto');
+module.exports.db=sqliteDB;
+module.exports.Client=Client;
+module.exports.Ticket=Ticket;
+
 /*
- *	Functions
+ *	DATABASE
  */
  
+function sqliteDB(file) {
+    
+	console.log("Opening file:"+file);
+	var exists = fs.existsSync(file);
+
+	if( !exists )
+	{
+		console.log("Creating DB file: " + file );
+		fs.openSync(file, "w");
+	}
+
+	ticketConn = new sqlite3.Database(file,function() {
+		ticketConn.run('PRAGMA foreign_keys=on');
+    });
+
+	if( !exists )
+	{
+		// Força que todas as queries dentro do serialize, e ao mesmo nível,
+		// sejam executadas em série e não em paralelo.
+
+		ticketConn.serialize(function(){
+
+			ticketConn
+				.run("BEGIN;")
+
+				// Cria as tabelas
+			
+				.run("CREATE TABLE clients (cid INTEGER PRIMARY KEY, name TEXT NOT NULL, nib TEXT NOT NULL, salt TEXT NOT NULL, pass TEXT NOT NULL);")
+				.run("CREATE TABLE tickets (tid INTEGER PRIMARY KEY, type INTEGER NOT NULL,  cid REFERENCES clients(cid), INTEGER dateValidated TEXT NOT NULL, dateBought TEXT NOT NULL);")
+				
+				// Insere os dados na db
+
+				//.run("INSERT INTO clients (name) VALUES ('Diogo'), ('Nelson');")
+				//.run("INSERT INTO tickets (type,dateValidated,dateBought) VALUES ('Cheese',);")
+				
+			.run("COMMIT;");
+		});
+	}
+}
+
+sqliteDB.prototype.createClient=function(client,pass,callback)
+{
+	console.log("create client ",client.name);
+	if( typeof callback !== 'function')
+		throw new Error('Callback is not a function');
+	createHashPwd(pass,function(hashedPass,salt){
+		ticketConn.run("INSERT INTO clients (name,nib,salt,pass) VALUES (?,?,?,?);",
+		[client.name,client.nib,salt,hashedPass],
+		function(err){
+			console.log(pass," ",salt," ",hashedPass);
+			callback(err, this.lastID, this.changes);
+		});
+	});
+}
+
+sqliteDB.prototype.login=function(name,pass,callback)
+{
+	console.log("login client ",name);	
+	if( typeof callback !== 'function')
+		throw new Error('Callback is not a function');
+	ticketConn.all("SELECT cid,name,nib,salt,pass FROM clients WHERE name=?",
+	[name],
+	function(err, row) {
+		if( row && row.length > 0 )
+		{
+			row=row[0];
+			verifyPwd(pass,row.salt,row.pass,function(sucess){
+				if (sucess)callback(err,row );
+				else callback(err,null);
+			});
+
+		}
+		else
+			callback(err,null );
+	});
+}
+
  
 /*
  *	TICKET CLASS
  */
 
-function Ticket(){
-	
+function Ticket(type,client){
+	this.id=null;
+	this.client=client;
+	this.type=type;
+	this.dateValidated="";
+	this.dateBought="";
 }
 
-Ticket.prototype.list=function(client){
-	return client;
+/*
+ *	CLIENT CLASS
+ */ 
+ 
+function Client(name){
+	this.id=null;
+	this.name=name;
+	this.tickets=[];
+	this.nib=null;
+}
+
+
+
+
+/*
+ *	Functions
+ */
+
+//get time
+function timeNow()
+{
+	return ( new Date() / 1000 ) | 0 ;
+}
+
+//createHashPwd
+//create a password salt and hash it.
+//args: pass -> password string, callback->callback with password and salt argument
+function createHashPwd(pass,callback)
+{
+	var salt=crypto.randomBytes(128).toString('base64');
+	crypto.pbkdf2(pass, salt , 100, 512, function(err,hashPass){
+		if (err) console.log("create hash error ",err);
+		callback(hashPass.toString('base64'),salt.toString('base64'));
+	});
+}
+
+
+function verifyPwd(pass,salt,hashed,callback)
+{
+	crypto.pbkdf2(pass, salt , 100, 512, function(err,hashPass){
+		if (hashPass.toString('base64')==hashed&&!err)callback(true);
+		else 
+		{
+			if (err) console.log("verify hash error ",err);
+			callback(false);
+		}
+	});
 }
